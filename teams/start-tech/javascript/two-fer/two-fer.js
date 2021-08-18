eab4e1b69301efcd4b223a76f28886d8a00649ab
Gabriela Moro Berte <gabriela.berte@hbsis.com.br>
2021-08-18 20:45:22 -0300

    exercicio 2

---
+++ teams/start-tech/javascript/two-fer/two-fer.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (nome) => {
+  if 
+    (nome == undefined) return 'One for you, one for me.';
+  else 
+    return `One for ${nome}, one for me.`; //pode colocar "One for" +nome+,", one for me."
+};
+
