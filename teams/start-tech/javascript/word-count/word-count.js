23d0b53743f762b2093b2a931cb3ff087623535e
gabriela.berte <you@example.com>
2021-08-26 16:52:45 -0300

    word count

--- teams/start-tech/javascript/word-count/word-count.js
+++ teams/start-tech/javascript/word-count/word-count.js
@@ -1,8 +1,21 @@
-/*
-
+/* ('\w+) -> junta o ' com a proxima letra para incluir como palavra senao a letra ia ficar separada
+(\w+) -> separa por palavras
+* 0 ou mais do que foi colocado
+------------------------------------------------------------------------------------------------
+O método match() retorna uma correspondência entre uma string com uma expressão regular.
+str.match(regexp)
 */
 
-export const countWords = (texto) => {
-  let palavras = texto.split(/\s/);
-  console.log(palavras)
+export const countWords = (frase) => {
+  let fraseMinuscula = frase.toLowerCase() //transformando em letra minuscula
+  let palavrasSeparadas = fraseMinuscula.match(/\w+('\w+)?/g);//separando a string por palavras
+
+  let resultado = {}; //criando objeto vazio pro contador mandar os resultados
+  for (const palavra of palavrasSeparadas) { //vai contar as palavras repetidas
+
+      resultado[palavra] ??= 0; //se for nulo ou indefinido atribua 0 e manda pro obj
+      resultado[palavra] += 1; //se não, adiciona 1 a cada palavra igual e manda pro obj
+    
+  };
+  return resultado;
 };
