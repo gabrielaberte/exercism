1ec34bf06184f14e72a215ab3eef2e66e6200b31
gabriela.berte <you@example.com>
2021-08-30 08:57:44 -0300

    raindrops

---
+++ teams/start-tech/javascript/raindrops/raindrops.js
@@ -0,0 +1,24 @@
+// - has 3 as a factor, add 'Pling' to the result.
+// - has 5 as a factor, add 'Plang' to the result.
+// - has 7 as a factor, add 'Plong' to the result.
+// - _does not_ have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+
+// ## Examples
+
+// - 28 has 7 as a factor, but not 3 or 5, so the result would be "Plong".
+// - 30 has both 3 and 5 as factors, but not 7, so the result would be "PlingPlang".
+// - 34 is not factored by 3, 5, or 7, so the result would be "34".
+
+export const convert = (numero) => {
+ var rainDrop ="";
+ if (numero%3 == 0){rainDrop = rainDrop + "Pling"};
+ if (numero%5 == 0){rainDrop = rainDrop + "Plang"};
+ if (numero%7 == 0){rainDrop = rainDrop + "Plong"};
+ if (numero%3 !== 0 && numero%5 !==0 && numero%7 !==0) {rainDrop = "" + numero}
+ return rainDrop;
+};
+
+
+
+
+
