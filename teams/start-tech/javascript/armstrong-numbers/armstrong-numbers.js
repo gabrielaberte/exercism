3c5bb2b7688e5c5bec13ee6d577be2e16589601c
gabriela.berte <you@example.com>
2021-08-30 11:26:43 -0300

    armstrong number

--- teams/start-tech/javascript/armstrong-numbers/armstrong-numbers.js
+++ teams/start-tech/javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,9 +1,25 @@
 export const isArmstrongNumber = (numero) => {
-    const string = `${numero}`;
-    let soma = 0;
-    // Numero de Armstrong = cada elelemento elevado ao número de elementos
-    for (let i = 0; i < string.length; i++) {
-        soma += parseInt(string[i]) ** string.length;
-    }
-    return numero === soma;
-};
+    //transformando numero em array
+    var numeroSeparado = numero.toString().match(/[0-9]/g); 
+
+    let soma = 0 //somando cada elemento do array elevado ao tamanho do array
+    for (let index = 0; index < numeroSeparado.length; index++) {
+            soma += numeroSeparado[index]**numeroSeparado.length;
+        };
+    if (soma === numero) return true; //se a soma for igual o numero ele é armstrong
+    else return false;
+
+    };
+   
+   
+   
+   
+
+   
+    // const string = `${numero}`;
+    // let soma = 0;
+    // // Numero de Armstrong = cada elelemento elevado ao número de elementos
+    // for (let i = 0; i < string.length; i++) {
+    //     soma += parseInt(string[i]) ** string.length;
+    // }
+    // return numero === soma;
